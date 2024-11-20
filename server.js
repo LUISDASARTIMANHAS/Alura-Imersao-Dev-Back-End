2b09075fe05a4bbc3de36a02bf1e6500728631a9
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-11-20 14:27:13 -0300

    UPDATED AULA 2

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 import express from "express";
 const app = express();
+const posts = [
+    {
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150",
+        id: 1
+      },
+      {
+        descricao: "gato fazendo yoga",
+        imagem: "https://placecats.com/millie/300/150",
+        id: 2
+      },
+      {
+        descricao: "gato fazendo panqueca",
+        imagem: "https://placecats.com/millie/300/150",
+        id: 3
+      }
+];
+app.use(express.json());
+
+function buscarPostPorId(id){
+    return posts.findIndex((post)=>{
+        return post.id === Number(id)
+    });
+}
 
 app.listen(3000,() =>{
     console.log("Servidor Iniciado!");
 });
 
-app.get("/api",(req,res)=>{
-    res.status(200).send("Boas Vindas a Imersão");
+app.get("/posts",(req,res)=>{
+    res.status(200).json(posts);
+});
+
+app.get("/posts/:id",(req,res)=>{
+    const index = buscarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
